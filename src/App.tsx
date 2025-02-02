f039cff13182a6312299cba7ec293a691f6c165c
poseidon0z <adityaprabhu1290@gmail.com>
2025-02-02 20:10:53 +0530

    Added basic site stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import clubLogo from '/Club Logo.png';
+import swords from '/Swords.png';
+
 function App() {
-  return <div className="text-blue-600 text-3xl font-bold">Hi</div>;
+  return (
+    <>
+      <div className="w-screen min-h-screen backdrop-blur-[1px]">
+        <div className="flex justify-center items-center m-5 p -5">
+          <img src={clubLogo} className="h-20"></img>
+          <h1 className="text-white text-4xl font-extrabold m-5 p-3">
+            Syntax Showdown
+          </h1>
+          <img src={swords} className="h-18"></img>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
