07dad65de5b8707f436946e9e16656fb6e6fab7f
Aneesh Kolar <kolaraneesh2003@gmail.com>
2025-02-03 00:48:19 +0530

    layer 1 and added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Box from './graph/Box';
 import clubLogo from '/Club Logo.png';
 import swords from '/Swords.png';
+import { SteppedLineTo } from 'react-lineto';
 
 function App() {
   return (
@@ -13,13 +14,167 @@ function App() {
           </h1>
           <img src={swords} className="h-18"></img>
         </div>
-        <Box
-          player1="adi"
-          player2="aneesh"
-          color1="#d73546"
-          color2="#376189"
-          color3="#ffffff"
-        />
+
+        <div className='flex'>
+          
+          <div>
+
+            <div className='m-10 A'>
+              <Box
+                player1="adi"
+                player2="aneesh"
+                color1="#d73546"
+                color2="#376189"
+                color3="#ffffff"
+              />
+            </div>
+
+            <div className='m-10 B'>
+              <Box
+                player1="adi"
+                player2="aneesh"
+                color1="#d73546"
+                color2="#376189"
+                color3="#ffffff"
+              />
+            </div>
+
+          </div>
+
+            <div className='m-30 A'>
+                <Box
+                    player1="adi"
+                    player2="aneesh"
+                    color1="#d73546"
+                    color2="#376189"
+                    color3="#ffffff"
+                  />
+            </div>
+          <SteppedLineTo from='m-10 A' zIndex={0} borderColor='white' to='m-30 A' orientation='h' borderWidth={10} />
+          <SteppedLineTo from='m-10 B' borderColor='white' zIndex={0} to='m-30 A' orientation='h' borderWidth={10} />
+        </div>
+
+        <div>
+          <div className='flex'>
+            
+            <div>
+
+              <div className='m-10 C'>
+                <Box
+                  player1="adi"
+                  player2="aneesh"
+                  color1="#d73546"
+                  color2="#376189"
+                  color3="#ffffff"
+                />
+              </div>
+
+              <div className='m-10 D'>
+                <Box
+                  player1="adi"
+                  player2="aneesh"
+                  color1="#d73546"
+                  color2="#376189"
+                  color3="#ffffff"
+                />
+              </div>
+
+            </div>
+
+              <div className='m-30 B'>
+                  <Box
+                      player1="adi"
+                      player2="aneesh"
+                      color1="#d73546"
+                      color2="#376189"
+                      color3="#ffffff"
+                    />
+              </div>
+            <SteppedLineTo from='m-10 C' zIndex={0} borderColor='white' to='m-30 B' orientation='h' borderWidth={10} />
+            <SteppedLineTo from='m-10 D' borderColor='white' zIndex={0} to='m-30 B' orientation='h' borderWidth={10} />
+          </div>
+
+          <div>
+            <div className='flex'>
+              <div>
+
+                <div className='m-10 E'>
+                  <Box
+                    player1="adi"
+                    player2="aneesh"
+                    color1="#d73546"
+                    color2="#376189"
+                    color3="#ffffff"
+                  />
+                </div>
+
+                <div className='m-10 F'>
+                  <Box
+                    player1="adi"
+                    player2="aneesh"
+                    color1="#d73546"
+                    color2="#376189"
+                    color3="#ffffff"
+                  />
+                </div>
+
+              </div>
+
+                <div className='m-30 C'>
+                    <Box
+                        player1="adi"
+                        player2="aneesh"
+                        color1="#d73546"
+                        color2="#376189"
+                        color3="#ffffff"
+                      />
+                </div>
+              <SteppedLineTo from='m-10 E' zIndex={0} borderColor='white' to='m-30 C' orientation='h' borderWidth={10} />
+              <SteppedLineTo from='m-10 F' borderColor='white' zIndex={0} to='m-30 C' orientation='h' borderWidth={10} />
+            </div>
+          </div>
+
+          <div>
+          <div className='flex'>
+              <div>
+
+                <div className='m-10 G'>
+                  <Box
+                    player1="adi"
+                    player2="aneesh"
+                    color1="#d73546"
+                    color2="#376189"
+                    color3="#ffffff"
+                  />
+                </div>
+
+                <div className='m-10 H'>
+                  <Box
+                    player1="adi"
+                    player2="aneesh"
+                    color1="#d73546"
+                    color2="#376189"
+                    color3="#ffffff"
+                  />
+                </div>
+
+              </div>
+
+                <div className='m-30 D'>
+                    <Box
+                        player1="adi"
+                        player2="aneesh"
+                        color1="#d73546"
+                        color2="#376189"
+                        color3="#ffffff"
+                      />
+                </div>
+              <SteppedLineTo from='m-10 G' zIndex={0} borderColor='white' to='m-30 D' orientation='h' borderWidth={10} />
+              <SteppedLineTo from='m-10 H' borderColor='white' zIndex={0} to='m-30 D' orientation='h' borderWidth={10} />
+            </div>
+          </div>
+        </div>
+
       </div>
     </>
   );
